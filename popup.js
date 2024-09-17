596295c9f6476c844aa45865f159e9538b89345d
Nova <69091361+fl2on@users.noreply.github.com>
2024-09-17 17:05:39 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
     document.addEventListener("DOMContentLoaded", function () {
       var currentYear = new Date().getFullYear();
       var copyrightElement = document.createElement("p");
-      copyrightElement.textContent = "Copyright © " + currentYear + " Qzxtu";
+      copyrightElement.textContent = "Copyright © " + currentYear + " fl2on";
       document.querySelector(".popup").appendChild(copyrightElement);
     });
